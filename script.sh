c2ba80cabd656e8b16898431389f735b1363fcd4
BD103 <59022059+BD103@users.noreply.github.com>
2021-01-28 11:15:47 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,11 @@
 echo $ACTIONTYPE
+if [ $ACTIONTYPE == "lint" ]; then
+  pip install -U black isort flake8
+  isort . -c --profile black
+  flake8 .
+  black . --check
+elif [ $ACTIONTYPE == "test" ]; then
+  pip install -U pytest
+  pytest
+else; then
+  echo "Invalid actionType Config"
